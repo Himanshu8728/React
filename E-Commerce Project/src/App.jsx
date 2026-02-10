c4b1b5f5c588fd9ccc1c63189d6f1e5aa0a69041
Himanshu Mourya <119477557+Himanshu8728@users.noreply.github.com>
2026-02-10 12:32:10 +0530

    React-Router

--- E-Commerce Project/src/App.jsx
+++ E-Commerce Project/src/App.jsx
@@ -1,11 +1,14 @@
+import { Routes, Route } from 'react-router'
 import { HomePage } from './Pages/HomePage'
 import './App.css'
 
 function App() {
-  
-
-  return (
-    <HomePage />
+  return ( 
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/checkout" element={<div>test checkout page</div>}></Route>
+    </Routes>
+    
   
   )
 }
