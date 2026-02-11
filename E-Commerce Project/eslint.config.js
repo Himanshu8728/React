ea91b658612acdcd72a4163f5170c628dceb3229
Himanshu Mourya <119477557+Himanshu8728@users.noreply.github.com>
2026-02-11 15:39:10 +0530

    commmiting something

--- E-Commerce Project/eslint.config.js
+++ E-Commerce Project/eslint.config.js
@@ -4,6 +4,24 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node }
+    }
+  },
+
   pluginReact.configs.flat.recommended,
+
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+
+      // ✅ ADD THIS
+      "no-unused-vars": "warn"
+    }
+  }
 ]);
--- React-Basics/react-basics.js
+++ React-Basics/react-basics.js
@@ -1,1 +1,37 @@
-console.log('hello')
+//Reverse String
+let str = 'text_string'
+let ReverseString = str.split('').reverse().join('')
+//console.log(ReverseString)
+
+function IsPalindrome(str){
+    let Reverse = str.split('').reverse().join('')
+    if(str==Reverse){
+        console.log('true')
+    }else{
+        console.log('false')
+    }
+}
+
+//Palindrome or Not
+//IsPalindrome('LOL')
+
+
+//Maximum number in array
+let Arr = [1, 2, 5, 4, 6, 7, 2]
+let MaximumNumber = Arr.sort().reverse()
+//console.log(MaximumNumber[0])
+
+const points = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){return a - b});
+//console.log(points)
+
+const Numbers = [1, 23, 56, 45, 75, 34]
+const maxNumber = Math.max(...Numbers)
+//console.log(maxNumber)
+
+//Remove Dublicate
+const myArray = [1, 2, 3, 3, 4, 56, 4, 2, 3]
+let set = new Set(myArray)
+console.log(set)
+
+
