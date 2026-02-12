0a3e28fa3c3a7cd080fb6fe974ff739f8f11bcb6
Himanshu Mourya <119477557+Himanshu8728@users.noreply.github.com>
2026-02-12 12:59:09 +0530

    Life cart up and share it betwee pages

--- E-Commerce Project/src/App.jsx
+++ E-Commerce Project/src/App.jsx
@@ -1,4 +1,6 @@
+import axios from 'axios'
 import { Routes, Route } from 'react-router'
+import { useState, useEffect } from 'react'
 import { HomePage } from './Pages/HomePage'
 import { CheckoutPage } from './Pages/CheckoutPage'
 import { OrdersPage } from './Pages/ordersPage'
@@ -6,10 +8,20 @@ import { TrackingPage } from './Pages/TrackingPage'
 import './App.css'
 
 function App() {
+  const [cart, setCart] = useState([])
+
+    useEffect(()=>{
+            axios.get('http://localhost:3000/api/cart-items')
+    .then((response)=>{
+      setCart(response.data);
+    })
+    }, [])
+
+
   return ( 
     <Routes>
-      <Route path="/" element={<HomePage />}></Route>
-      <Route path="/checkout" element={<CheckoutPage />}></Route>
+      <Route path="/" element={<HomePage cart={cart} />}></Route>
+      <Route path="/checkout" element={<CheckoutPage cart={cart} />}></Route>
       <Route path="/orders" element={<OrdersPage />}></Route>
       <Route path="/tracking" element={<TrackingPage />}></Route>
     </Routes>
