03ce0aa937c8c2860f72cbffae92535e0c93bbe6
Himanshu Mourya <119477557+Himanshu8728@users.noreply.github.com>
2026-02-17 12:05:08 +0530

    Create integration test for Product component

--- E-Commerce Project/src/Pages/home/Product.jsx
+++ E-Commerce Project/src/Pages/home/Product.jsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
-import { formatMoney } from "../../utils/money";
-import axios from "axios";
+import axios from 'axios';
+import { useState } from 'react';
+import { formatMoney } from '../../utils/money';
 
 export function Product({ product, loadCart }) {
   const [quantity, setQuantity] = useState(1);
 
   const addToCart = async () => {
-    await axios.post("/api/cart-items", {
+    await axios.post('/api/cart-items', {
       productId: product.id,
-      quantity,
+      quantity
     });
     await loadCart();
   };
@@ -19,24 +19,30 @@ export function Product({ product, loadCart }) {
   };
 
   return (
-    <div className="product-container">
+    <div className="product-container"
+      data-testid="product-container">
       <div className="product-image-container">
-        <img className="product-image" src={product.image} />
+        <img className="product-image"
+          data-testid="product-image"
+          src={product.image} />
       </div>
 
-      <div className="product-name limit-text-to-2-lines">{product.name}</div>
+      <div className="product-name limit-text-to-2-lines">
+        {product.name}
+      </div>
 
       <div className="product-rating-container">
-        <img
-          className="product-rating-stars"
-          src={`images/ratings/rating-${product.rating.stars * 10}.png`}
-        />
+        <img className="product-rating-stars"
+          data-testid="product-rating-stars-image"
+          src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
         <div className="product-rating-count link-primary">
           {product.rating.count}
         </div>
       </div>
 
-      <div className="product-price">{formatMoney(product.priceCents)}</div>
+      <div className="product-price">
+        {formatMoney(product.priceCents)}
+      </div>
 
       <div className="product-quantity-container">
         <select value={quantity} onChange={selectQuantity}>
@@ -54,12 +60,15 @@ export function Product({ product, loadCart }) {
       </div>
 
       <div className="product-spacer"></div>
+
       <div className="added-to-cart">
         <img src="images/icons/checkmark.png" />
         Added
       </div>
 
-      <button className="add-to-cart-button button-primary" onClick={addToCart}>
+      <button className="add-to-cart-button button-primary"
+        data-testid="add-to-cart-button"
+        onClick={addToCart}>
         Add to Cart
       </button>
     </div>
